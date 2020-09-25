97cb91d81ee94f0d01325203f1a9ecfa7ef49bcb
ilievss <ilievss@abv.bg>
2020-09-25 12:22:06 +0300

    update the word validation

--- stringutils/validate.go
+++ stringutils/validate.go
@@ -3,7 +3,10 @@ package stringutils
 import "strings"
 
 const space = " "
+const nonWordCharacters = "1234567890`_=+[];\"\\|,./<>?!@#$%^&*()"
 
 func IsWord(word string) bool {
-	return len(word) > 0 && len(strings.Split(word, space)) == 1
+	return len(word) > 0 &&
+		len(strings.Split(word, space)) == 1 &&
+		strings.IndexAny(word, nonWordCharacters) == -1
 }

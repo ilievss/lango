f9cc741583a3a7814cc9437d0217a0c4e7f7e211
ilievss <ilievss@abv.bg>
2020-09-28 10:21:14 +0300

    add the port as an command line argument

--- main.go
+++ main.go
@@ -3,10 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/ilievss/lango/web"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	port := 8080
+	if len(os.Args) > 3 && !strings.Contains(os.Args[1], "-port") {
+		fmt.Println("Program accepts only one argument: -port")
+		os.Exit(0)
+	}
+
+	port, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Not a valid port: " + os.Args[2])
+		os.Exit(0)
+	}
+
+	if port < 1000 || port > 65535 {
+		fmt.Println("Port not in range [1000, 65535]: " + os.Args[2])
+		os.Exit(0)
+	}
+
 	addr := fmt.Sprintf(":%d", port)
 	server := web.New(addr)
 	server.Start()

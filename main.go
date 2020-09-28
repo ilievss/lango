3de1be4bf3992bc7dd33f57e48918ef54c67e451
ilievss <ilievss@abv.bg>
2020-09-28 16:43:23 +0300

    use the flag package to parse command line args

--- main.go
+++ main.go
@@ -1,25 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/ilievss/lango/web"
 	"os"
-	"strconv"
-	"strings"
 )
 
+const defaultPort = 8080
+
 func main() {
-	if len(os.Args) > 3 && !strings.Contains(os.Args[1], "-port") {
-		fmt.Println("Program accepts only one argument: -port")
-		os.Exit(0)
-	}
+	portFlag := flag.Int("port", defaultPort, "The port on which to start the server")
+	flag.Parse()
 
-	port, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		fmt.Println("Not a valid port: " + os.Args[2])
-		os.Exit(0)
-	}
-
+	port := *portFlag
 	if port < 1000 || port > 65535 {
 		fmt.Println("Port not in range [1000, 65535]: " + os.Args[2])
 		os.Exit(0)
